9a34a08e879f7456c5efc2fb2433a804fb9924cd
신다겸 <dakyeom127@gmail.com>
2019-08-21 00:23:14 +0900

    editor에 redux 설정

---
+++ src/frontend/src/store/modules/editor.js
@@ -0,0 +1,25 @@
+import { createAction, handleActions } from 'redux-actions';
+import { Map } from 'immutable';
+import { pender } from 'redux-pender';
+
+//action type
+const INITIALIZE = 'editor/INITIALIZE';
+const CHANGE_INPUT = 'editor/CHANGE_INPUT';
+
+//action constructor(액션 생성자)
+export const initialize = createAction(INITIALIZE);
+export const changeInpot = createAction(CHANGE_INPUT);
+
+//state reset(상태 초기화)
+const initialState = Map({
+    title: '',
+    markdown:''
+});
+
+export default handleActions({
+    [INITIALIZE]: (state, action) => initialState,
+    [CHANGE_INPUT]: (state, action) => {
+        const { name, value } = action.payload;
+        return state.set(name, value); 
+    }
+}, initialState)
