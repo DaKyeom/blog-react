dd23838f73677d060392b0d97883b3d401b35490
신다겸 <dakyeom127@gmail.com>
2019-08-21 00:25:01 +0900

    markdownEditor,preview Page완성하고  redux설정 코드 추가

--- src/frontend/src/client/Root.js
+++ src/frontend/src/client/Root.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import configure from 'store/configure';
 import App from 'shared/App';
 
+const store = configure();
+
 const Root = () => (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 export default Root;
--- src/frontend/src/components/editor/EditorPane.js
+++ src/frontend/src/components/editor/EditorPane.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import './EditorPane.scss';
 
+import CodeMirror from 'codemirror';
+
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/shell/shell';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
+
 class EditorPane extends Component {
+    editor = null;
+    CodeMirror =null;
+    
+    initalizeEditor = () => {
+        this.CodeMirror = CodeMirror(this.editor, {
+            mode: 'markdown',
+            theme: 'monokai',
+            lineNumbers: true,
+            lineWrapping: true
+        });
+    }
+
+    componentDidMount() {
+        this.initalizeEditor();
+    }
+
     render(){
         return (
           <div className="editor-pane">
               <input className='title' placeholder='제목입력' name='title' />
-              <div className='code-editor'></div>
+              <div className='code-editor' ref={ref => this.editor=ref}></div>
           </div>
-        )
+        );
     }
 }
 
